9e442ca12f89d5c894cf2c24b54e9936a2472011
Jainish Kunjadiya <jainishkunjadiya2712@gmail.com>
2024-08-03 04:50:25 +0530

    Added End points for Login and SignUp

--- src/main/java/com/app/restrobuddy/Controller/UserController.java
+++ src/main/java/com/app/restrobuddy/Controller/UserController.java
@@ -1,9 +1,12 @@
 package com.app.restrobuddy.Controller;
 
 import com.app.restrobuddy.FeignClient.UserFeignClient;
+import com.app.restrobuddy.Shared.Dtos.LoginDto;
 import com.app.restrobuddy.Shared.Dtos.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserFeignClient userFeignClient;
@@ -20,9 +24,13 @@ public class UserController {
         this.userFeignClient = feignClient;
     }
 
+    @PostMapping("/save-user")
+    public UserDto saveUser(@RequestBody UserDto userDto){
+        return userFeignClient.saveUser(userDto);
+    }
 
-    @GetMapping("/findAll")
-    public List<UserDto> findAll(){
-        return userFeignClient.getAllUser();
+    @PostMapping("/login")
+    public UserDto login(@RequestBody LoginDto loginDto){
+        return userFeignClient.login(loginDto);
     }
 }
--- src/main/java/com/app/restrobuddy/RestroBuddyApplication.java
+++ src/main/java/com/app/restrobuddy/RestroBuddyApplication.java
@@ -3,6 +3,7 @@ package com.app.restrobuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableFeignClients(basePackages = "com.app.restrobuddy.FeignClient")
 @SpringBootApplication
